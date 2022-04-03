58ec3ab4de30dafea75fcaa5c5804b2e73473805
David Fant <david@fant.io>
2022-04-04 01:54:13 +0200

    updated fonts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,33 +7,28 @@ const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const rglr = readFileSync(`${__dirname}/../_fonts/AvenirNextLTPro-Regular.otf`).toString('base64');
+const bold = readFileSync(`${__dirname}/../_fonts/AvenirNextLTPro-Bold.otf`).toString('base64');
+// const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 const background = readFileSync(`${__dirname}/../../public/background.png`).toString('base64');
 
 function getCss(_theme: string, fontSize: string) {
     return `
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&display=swap');
+
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Avenir';
         font-style:  normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
 
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Avenir';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
-
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
 
     body {
         background: url(data:image/png;base64,${background});
@@ -47,7 +42,7 @@ function getCss(_theme: string, fontSize: string) {
 
     code {
         color: #D400FF;
-        font-family: 'Vera';
+        font-family: monospace;
         white-space: pre-wrap;
         letter-spacing: -5px;
     }
@@ -90,7 +85,7 @@ function getCss(_theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', sans-serif;
+        font-family: 'Avenir', 'Noto Sans SC', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: white;
@@ -98,11 +93,11 @@ function getCss(_theme: string, fontSize: string) {
     }
     
     .subtitle {
-        font-family: 'Inter', sans-serif;
-        font-size: 60px;
+        font-family: 'Avenir', 'Noto Sans SC', sans-serif;
+        font-size: 80px;
         font-style: normal;
         color: white;
-        opacity: 50%;
+        opacity: 70%;
         line-height: 1.3;
     }`;
 }
